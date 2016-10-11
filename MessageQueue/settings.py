965478f481a01db0de06505101fec3e19c8f94cb
gunendu <gunendu@gmail.com>
2016-10-11 20:17:11 +0530

    static file path change

--- MessageQueue/settings.py
+++ MessageQueue/settings.py
@@ -80,5 +80,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATIC_ROOT = "/var/www/html/"
